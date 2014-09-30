3940effc3d83bc1dbb3afae12d8403466a499a96
Nec Necsoft <necsoftdesign@gmail.com>
2014-09-30 01:03:48 -0300

    Agregadas las opciones para modificar el package

--- app/index.js
+++ app/index.js
@@ -9,9 +9,26 @@ module.exports = yeoman.generators.Base.extend({
     var prompts = [{
       name: 'appName',
       message: 'What is your project name?'
+    },{
+      name: 'description',
+      message: 'Description for the project'
+    },{
+      name: 'has_frame',
+      type: 'confirm',
+      message: 'Has frame?'
+    },{
+      name: 'has_toolbar',
+      type: 'confirm',
+      message: 'Has toolbar?'
+    },{
+      name : 'is_resizable',
+      type: 'confirm',
+      message: 'Is resizable?'
     }];
     this.prompt(prompts, function (props) {
       this.appName = props.appName;
+      this.description = props.description;
+      this.has_frame = props.has_frame;
       done();
     }.bind(this));
   },
@@ -31,11 +48,20 @@ module.exports = yeoman.generators.Base.extend({
     this.mkdir(this.appName+"/app/stylesheets/stylus");
   },
   copy_files: function(){
+    //Context for the templating
+    var context = {
+      site_name : this.appName,
+      description : this.description,
+      has_frame : this.has_frame,
+      has_toolbar: this.has_toolbar,
+      is_resizable: this.is_resizable,
+    };
+
     //Project Files
     this.copy("_gruntfile.js", this.appName+"/gruntfile.js");
-    this.copy("_package.json", this.appName+"/package.json");
+    this.template("_package.json", this.appName+"/package.json",context);
     this.copy("_README.md", this.appName+"/README.md");
-    this.copy("_.gitignore",this.appName+"/.gitignore")
+    this.copy("_.gitignore",this.appName+"/.gitignore");
 
     //Node-webkit
     this.copy("_nw.js", this.appName+"/app/nw.js");
