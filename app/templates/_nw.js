e7f7f5d5c863d8a0ec1306614c2e715ce289d65d
Nec Necsoft <necsoftdesign@gmail.com>
2014-09-30 01:08:17 -0300

    Agregado el nw.js

--- app/templates/_nw.js
+++ app/templates/_nw.js
@@ -1,1 +1,15 @@
-// Aca va la inicializacion de nw
+//
+// nw.js
+// -------------------------------------------------------
+//
+// This file is used for the initialization of node-webkit
+//
+
+// nw.gui initialization
+var nw = window.require("nw.gui");
+var win = nw.Window.get();
+
+// Set the devmode
+if(nw.App.argv.slice(0) == "devmode"){
+  win.showDevTools();
+}
